929d2cdb9779bec143a420689b153c642671c115
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-25 09:27:21 -0400

    declaring express app variable along with PORT. Also setting up session to connect to sequelize db

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('.connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
