a0f2fe9ac12486e13b0ca14e7d6f566c9426490f
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-27 14:41:33 -0400

    fixing syntax error for SequelizeStore

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('.connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
