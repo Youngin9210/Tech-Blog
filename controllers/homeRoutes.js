e80b525614f3e9a4e4fb6bb1d4d6fbf84f769020
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-27 16:07:47 -0400

    adding get route for login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,4 +9,8 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/login', async (req, res) => {
+  return req.session.loggedIn ? res.redirect('/') : res.render('login');
+});
+
 module.exports = router;
