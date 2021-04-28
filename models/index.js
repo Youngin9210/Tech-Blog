e39fd9e42570a597bdb328b38b4443741079eca6
Kyle <kyleyoung.9210@gmail.com>
2021-04-28 17:12:14 -0400

    updating associations

--- models/index.js
+++ models/index.js
@@ -5,28 +5,25 @@ const Comment = require('./Comment');
 // user hasMany blogs
 User.hasMany(Blog, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
-// user hasMany comments
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
 });
 // blog belongsTo user
 Blog.belongsTo(User, {
   foreignKey: 'user_id',
-});
-// blogs hasMany comments
-Blog.hasMany(Comment, {
-  foreignKey: 'blog_id',
-  onDelete: 'CASCADE',
 });
 // comments belongsTo user
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
 });
 // comments belongsTo post
-Comment.belongsTo(Post, {
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+});
+// user hasMany comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+// blogs hasMany comments
+Blog.hasMany(Comment, {
   foreignKey: 'blog_id',
 });
 
