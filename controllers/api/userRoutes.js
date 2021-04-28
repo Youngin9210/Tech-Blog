225a09786f560b5e0e0276f5ceff58f71c4339b2
Kyle <kyleyoung.9210@gmail.com>
2021-04-28 16:56:34 -0400

    successfully creating a new user with router.post() route

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.username;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+    console.log(userData);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+module.exports = router;
