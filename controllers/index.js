b0084d218584e12b022488f11966d9e0648d4b71
Kyle <kyleyoung.9210@gmail.com>
2021-04-29 17:11:09 +0000

    removed router.use() for 404 status

--- controllers/index.js
+++ controllers/index.js
@@ -6,8 +6,4 @@ const homeRoutes = require('./homeRoutes');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
-
 module.exports = router;
