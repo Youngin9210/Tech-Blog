68a362766ac257c87520cca948ecd09c2bef6c88
Kyle <kyleyoung.9210@gmail.com>
2021-04-30 15:56:29 -0400

    created js file to handle adding a comment to a blog

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
-const { Blog, User, Comment } = require('../../models');
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.create(req.body);
+    console.log(commentData);
+    res.status(200).json(commentData);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
 
 module.exports = router;
