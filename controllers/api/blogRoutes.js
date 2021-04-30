05785b4f178eaea595786a8329f1fb9a2eed19ff
Kyle <kyleyoung.9210@gmail.com>
2021-04-30 15:53:08 -0400

    created a PUT route to update blog

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/:id', async (req, res) => {
   try {
@@ -19,12 +20,25 @@ router.get('/:id', async (req, res) => {
     const blog = blogData.get({ plain: true });
     const comments = commentData.map((comment) => comment.get({ plain: true }));
     // console.log(blogData);
-    console.log(blog);
-    console.log('-----------------');
-    console.log(comments);
+    // console.log(blog);
+    // console.log('-----------------');
+    // console.log(comments);
     res.render('blog', { blog, comments, logged_in: req.session.logged_in });
   } catch (e) {
     console.log(e);
+  }
+});
+
+router.put('/update', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.Update(req.body, {
+      where: {
+        id: req.body.id,
+      },
+    });
+    res.status(200).json(blogData);
+  } catch (e) {
+    res.status(400).json(e);
   }
 });
 
