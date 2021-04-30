d299b1a98b5e91d50e8f04c2f70bdeb92661c083
Kyle <kyleyoung.9210@gmail.com>
2021-04-30 12:31:54 -0400

    added comment data for seed data for testing

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
 const { User, Blog, Comment } = require('../models');
 
-const userData = require('./userData.json');
+const userData = require('./userData');
 const blogData = require('./blogData');
+const commentData = require('./commentData');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,20 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  // const blogs = blogData;
+
   for (const blog of blogData) {
     await Blog.create({
       ...blog,
       user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      blog_id: Math.floor(Math.random() * blogData.length),
     });
   }
   process.exit(0);
