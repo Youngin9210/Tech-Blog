c6e584d8367507e1e3f8366dfc21994c27002457
Kyle <kyleyoung.9210@gmail.com>
2021-04-30 13:25:36 -0400

    including User in blogData

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,14 @@ const { User, Blog, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const blogData = await Blog.findAll();
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
     res.render('homepage', { blogs, logged_in: req.session.logged_in });
   } catch (e) {
