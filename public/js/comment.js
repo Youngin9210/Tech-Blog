fbba7df9aa6beeb64be0cd0c22fd1cc50b1d3f90
Kyle <kyleyoung.9210@gmail.com>
2021-04-30 15:50:51 -0400

    renamed variable. having issues with fetch route.

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,22 @@
 const newComment = async (e) => {
   e.preventDefault();
 
-  const commentText = document.querySelector('#comment-text').value.trim();
-  const user = document
+  const comment_content = document.querySelector('#comment-text').value.trim();
+  const user_id = document
     .querySelector('#submit-comment')
     .getAttribute('data-user');
-  const blog = document
+  const blog_id = document
     .querySelector('#submit-comment')
     .getAttribute('data-blog');
 
-  if (commentText) {
-    const res = await fetch('api/comment/create', {
+  if (comment_content) {
+    const response = await fetch('/api/comments', {
       method: 'POST',
-      body: JSON.stringify({ commentText, user_id, blog_id }),
+      body: JSON.stringify({ comment_content, user_id, blog_id }),
       headers: { 'Content-Type': 'application/json' },
     });
 
-    res.ok
+    response.ok
       ? document.location.replace(`/api/blogs/${blog_id}`)
       : alert('Failed to submit comment');
   }
